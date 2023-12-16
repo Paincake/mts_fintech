5755f057ea814e9ec2a2709dbe71ba0025bf4656
Paincake <ryazhev2012@gmail.com>
2023-12-17 00:57:28 +0300

    third homework done

---
+++ src/main/java/ru/mts/thirdhw/entity/AbstractAnimal.java
@@ -0,0 +1,11 @@
+package ru.mts.thirdhw.entity;
+
+import java.math.BigDecimal;
+
+public abstract class AbstractAnimal implements Animal {
+    protected String breed;
+    protected String name;
+    protected BigDecimal cost;
+    protected String character;
+
+}
