299e32a750ef34bf940d1cc6842fa9e05ab1b6ee
Paincake <ryazhev2012@gmail.com>
2023-12-23 19:57:58 +0300

    Main class code added

--- src/main/java/ru/mts/thirdhw/Main.java
+++ src/main/java/ru/mts/thirdhw/Main.java
@@ -1,11 +1,14 @@
 package ru.mts.thirdhw;
 
 import ru.mts.thirdhw.serviceimpl.CreateAnimalServiceImpl;
+import ru.mts.thirdhw.serviceimpl.SearchServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        CreateAnimalServiceImpl service = new CreateAnimalServiceImpl();
-        service.createAnimals();
-        service.createAnimals(100);
+        CreateAnimalServiceImpl creationService = new CreateAnimalServiceImpl();
+        SearchServiceImpl searchService = new SearchServiceImpl();
+        searchService.findDuplicate(creationService.createAnimals(5));
+        searchService.findLeapYearNames(creationService.createAnimals());
+        searchService.findOlderAnimal(creationService.createAnimals(10), 4);
     }
 }
--- src/main/java/ru/mts/thirdhw/util/DateGenerator.java
+++ src/main/java/ru/mts/thirdhw/util/DateGenerator.java
@@ -1,23 +1,31 @@
 package ru.mts.thirdhw.util;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DateGenerator {
     private static int randBetween(int start, int end) {
-        return start + (int)Math.round(Math.random() * (end - start));
+        return ThreadLocalRandom.current().nextInt(start, end + 1);
     }
 
     public static LocalDate generateDate() {
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.set(Calendar.YEAR, randBetween(1990, 2023));
-        gc.set(Calendar.DAY_OF_YEAR, randBetween(1, gc.getActualMaximum(Calendar.DAY_OF_YEAR)));
-        return LocalDate.of(
-                gc.get(Calendar.YEAR),
-                gc.get(Calendar.MONTH),
-                gc.get(Calendar.DAY_OF_MONTH)
-        );
+        Calendar gc = new GregorianCalendar();
+        int year = randBetween(1990, 2023);
+        int month = randBetween(1, 12);
+        gc.set(Calendar.YEAR, year);
+        gc.set(Calendar.MONTH, month);
+        gc.set(Calendar.DAY_OF_MONTH, randBetween(1, gc.getActualMaximum(Calendar.DAY_OF_MONTH)));
+        int day = gc.get(Calendar.DAY_OF_MONTH);
 
+        return LocalDate.of(
+                year,
+                month,
+                day
+        );
     }
 }
